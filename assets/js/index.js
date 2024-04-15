be6e0e5503ade7e9a7efb271493316022f4343c9
Mihaly Lovrencsics <lomysa@gmail.com>
2024-04-15 23:04:15 +0000

    Add logic to javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
-console.log("hello world!");
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hours = now.getHours();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 24) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(`${hours}h ${mins}mins ${seconds}seconds`);
+    console.log(`${hoursDegrees}hdeg ${minsDegrees}minsdeg ${secondsDegrees}secondsdeg`);
+    
+}
+
+setInterval(setDate, 1000, );
