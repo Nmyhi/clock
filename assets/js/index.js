f72db57a49aad5c0fa65390e5a10f05c724f2296
Mihaly Lovrencsics <lomysa@gmail.com>
2024-04-16 07:42:31 +0000

    Correct hour arm position calculation

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ function setDate(){
     const hours = now.getHours();
     const minsDegrees = ((mins / 60) * 360) + 90;
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    const hoursDegrees = ((hours / 24) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
